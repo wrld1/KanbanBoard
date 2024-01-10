8f2c27581152e782a252607b8440c3b82ed7b028
Kyrylo Lolenko <kir260302@gmail.com>
2024-01-10 02:54:08 +0200

    Change the length of Board name property

--- src/board/dto/create-board.dto.ts
+++ src/board/dto/create-board.dto.ts
@@ -2,6 +2,6 @@ import { IsNotEmpty, Length } from 'class-validator';
 
 export class CreateBoardDto {
   @IsNotEmpty()
-  @Length(5, 255, { message: 'The name length should be between 5 and 255' })
+  @Length(3, 255, { message: 'The name length should be between 3 and 255' })
   name: string;
 }
